6bd8c7edcefb4b2bab27e53e56a6dcdfe41a8970
chiragrathi24 <55191179+chiragrathi24@users.noreply.github.com>
2019-10-04 01:25:55 +0530

    Add files via upload

---
+++ Basic Calculator.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a, b, sum;
+	cout<<"Enter the two numbers to find their sum:"<<endl;
+	cin>>a>>b;
+	sum=a+b;
+	cout<<"The sum of the two numbers is:"<<sum<<endl;
+	return 0;
+}
